1a5d4871a9c954a7488f099b5267fd8b904a29d7
zzl <86543402@qq.com>
2021-11-28 00:39:40 +0800

    chore: 输出apk自定义名称

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,9 +73,33 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            // 打模拟器包时去掉
-            ndk {
-                abiFilters.addAll(setOf("arm64-v8a"))
+            splits {
+                abi {
+                    // Enables building multiple APKs per ABI.
+                    isEnable = true
+
+                    // By default all ABIs are included, so use reset() and include to specify that we only
+                    // want APKs for x86 and x86_64.
+
+                    // Resets the list of ABIs that Gradle should create APKs for to none.
+                    reset()
+
+                    // Specifies a list of ABIs that Gradle should create APKs for.
+                    include("arm64-v8a", "armeabi-v7a", "x86_64")
+
+                    // Specifies that we do not want to also generate a universal APK that includes all ABIs.
+                    isUniversalApk = false
+                }
+            }
+
+
+            android.applicationVariants.all {
+                outputs.all {
+                    if (this is com.android.build.gradle.internal.api.ApkVariantOutputImpl) {
+                        this.outputFileName =
+                            "findroid-compose-${name}-v$versionName.apk"
+                    }
+                }
             }
         }
         create("staging") {
